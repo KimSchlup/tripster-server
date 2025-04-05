e2a9d677a6b53cd322d52abcdc3879f786513d2f
Adrian Hauser <AdrianHauser@users.noreply.github.com>
2025-04-05 15:29:41 +0200

    #21: modified tests

--- src/test/java/ch/uzh/ifi/hase/soprafs24/repository/RoadtripRepositoryIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/repository/RoadtripRepositoryIntegrationTest.java
@@ -1,42 +1,41 @@
-// package ch.uzh.ifi.hase.soprafs24.repository;
+package ch.uzh.ifi.hase.soprafs24.repository;
 
-// import ch.uzh.ifi.hase.soprafs24.entity.Roadtrip;
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-// import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-// import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import ch.uzh.ifi.hase.soprafs24.entity.Roadtrip;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-// @DataJpaTest
-// @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-// public class RoadtripRepositoryIntegrationTest {
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class RoadtripRepositoryIntegrationTest {
 
-//   @Autowired
-//   private TestEntityManager entityManager;
+  @Autowired
+  private TestEntityManager entityManager;
 
-//   @Autowired
-//   private RoadtripRepository roadtripRepository;
+  @Autowired
+  private RoadtripRepository roadtripRepository;
 
-//   @Test
-//   public void findByName_success() {
-//     // given
-//     Roadtrip roadtrip = new Roadtrip();
-//     roadtrip.setName("Test Name");
-//     roadtrip.setDescription("Test Description");
+  @Test
+  public void findByName_success() {
+    // given
+    Roadtrip roadtrip = new Roadtrip();
+    roadtrip.setName("Test Name");
+    roadtrip.setDescription("Test Description");
 
-//     entityManager.persist(roadtrip);
-//     entityManager.flush();
+    entityManager.persist(roadtrip);
+    entityManager.flush();
 
-//     // when
-//     Roadtrip found = roadtripRepository.findById(roadtrip.getId()).orElse(null);
+    // when
+    Roadtrip found = roadtripRepository.findById(roadtrip.getId()).orElse(null);
 
-//     // then
-//     assertNotNull(found.getId());
-//     assertEquals(found.getName(), roadtrip.getName());
-//     assertEquals(found.getDescription(), roadtrip.getDescription());
-
-//   }
-// }
+    // then
+    assertNotNull(found.getId());
+    assertEquals(found.getName(), roadtrip.getName());
+    assertEquals(found.getDescription(), roadtrip.getDescription());
+  }
+}
