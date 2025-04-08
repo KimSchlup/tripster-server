1e6b540408d7df8eeee4bbc9db3eb0ec673ece9a
Adrian Hauser <AdrianHauser@users.noreply.github.com>
2025-04-09 00:47:04 +0200

    #49: added DELETE roadtripmember endpoint, set some contstraints on POST roadtrip

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/RoadtripMemberController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/RoadtripMemberController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import ch.uzh.ifi.hase.soprafs24.entity.Roadtrip;
 import ch.uzh.ifi.hase.soprafs24.entity.RoadtripMember;
+import ch.uzh.ifi.hase.soprafs24.entity.User;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.RoadtripGetDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.RoadtripMemberGetDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.RoadtripMemberPostDTO;
@@ -22,6 +24,7 @@ import ch.uzh.ifi.hase.soprafs24.rest.dto.RoadtripPostDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.mapper.DTOMapper;
 import ch.uzh.ifi.hase.soprafs24.service.RoadtripMemberService;
 import ch.uzh.ifi.hase.soprafs24.service.RoadtripService;
+import ch.uzh.ifi.hase.soprafs24.service.UserService;
 
 /**
  * Roadtrip Controller
@@ -30,27 +33,42 @@ import ch.uzh.ifi.hase.soprafs24.service.RoadtripService;
  * The controller will receive the request and delegate the execution to the
  * RoadtripService and finally return the result.
  */
-
 @RestController
 public class RoadtripMemberController {
 
     private final RoadtripMemberService roadtripMemberService;
+    private final UserService userService;
 
-    RoadtripMemberController(RoadtripMemberService roadtripMemberService) {
+    RoadtripMemberController(RoadtripMemberService roadtripMemberService, UserService userService) {
         this.roadtripMemberService = roadtripMemberService;
+        this.userService = userService;
     }
 
-    @PostMapping("/roadtrips{roadtripId}/members")
+    @PostMapping("/roadtrips/{roadtripId}/members")
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
-    public RoadtripMemberGetDTO createRoadtripMember(@RequestBody RoadtripMemberPostDTO roadtripMemberPostDTO, @PathVariable Long roadtripId) {
+    public RoadtripMemberGetDTO createRoadtripMember(@RequestBody RoadtripMemberPostDTO roadtripMemberPostDTO, @PathVariable Long roadtripId, @RequestHeader("Authorization") String token) {
+
+        User invitingUser = userService.getUserByToken(token); // No need to verify if user exists, otherwise authentication
+                                                       // fails
         // convert API user to internal representation
-        RoadtripMember roadtripMemberInput = DTOMapper.INSTANCE
-                .convertRoadtripMemberPostDTOtoEntity(roadtripMemberPostDTO);
+        Long userId = roadtripMemberPostDTO.getUserId();
 
         // create roadtrip
-        RoadtripMember createdRoadtripMember = roadtripMemberService.createRoadtripMember(roadtripId, roadtripMemberInput);
+        RoadtripMember createdRoadtripMember = roadtripMemberService.createRoadtripMember(roadtripId, invitingUser, userId);
+
         // convert internal representation of user back to API
         return DTOMapper.INSTANCE.convertEntityToRoadtripMemberGetDTO(createdRoadtripMember);
     }
+
+
+    @DeleteMapping("/roadtrips/{roadtripId}/members/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseBody
+    public void deleteRoadtripMember(@PathVariable Long roadtripId, @PathVariable Long userId, @RequestHeader("Authorization") String token) {
+        User deletingUser = userService.getUserByToken(token); // No need to verify if user exists, otherwise authentication
+                                                       // fails
+        roadtripMemberService.deleteRoadtripMember(roadtripId, deletingUser, userId);
+    }
+
 }
