fc48a0a559baea757b6efe5b515349d40bd845cb
Adrian Hauser <AdrianHauser@users.noreply.github.com>
2025-04-12 19:46:37 +0200

    #130: added defaults to enums

--- src/main/java/ch/uzh/ifi/hase/soprafs24/constant/BasemapType.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/constant/BasemapType.java
@@ -1,5 +1,9 @@
 package ch.uzh.ifi.hase.soprafs24.constant;
 
 public enum BasemapType {
-    SATELLITE, SATELLITE_HYBRID, OPEN_STREET_MAP;
+    SATELLITE, SATELLITE_HYBRID, OPEN_STREET_MAP, DEFAULT;
+
+    public static BasemapType getDefault() {
+        return BasemapType.OPEN_STREET_MAP; 
+    }
 }
