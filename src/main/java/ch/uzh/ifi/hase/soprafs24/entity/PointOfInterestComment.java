f94302c7e1b0ce179d59043fde317fedb2374692
@ricoandrea.camenzind <ricoandrea.camenzind@uzh.ch>
2025-04-15 10:05:29 +0200

    started implementing POI comments

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/PointOfInterestComment.java
@@ -0,0 +1,58 @@
+package ch.uzh.ifi.hase.soprafs24.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "point_of_interest_comment")
+
+public class PointOfInterestComment implements Serializable{
+    
+    @ManyToOne
+    @JoinColumn(name = "poi_id")
+    private PointOfInterest poi;
+
+    @Column(nullable = false)
+    private Long authorId;
+
+    @Column(nullable = false)
+    private String comment;
+
+    @Column(nullable = false)
+    private LocalDate creationDate;
+
+public PointOfInterest getPoi() {
+        return poi;
+    }
+    
+    public void setPoi(PointOfInterest poi) {
+        this.poi = poi;
+    }
+    
+    public Long getAuthorId() {
+        return authorId;
+    }
+    
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+    
+    public String getComment() {
+        return comment;
+    }
+    
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+    
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+    
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+    
+}
