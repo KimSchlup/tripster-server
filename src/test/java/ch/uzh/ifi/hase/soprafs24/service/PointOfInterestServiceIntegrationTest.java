3d757d5ab9efc4e73ab29fee2f8872f88f6812df
@ricoandrea.camenzind <ricoandrea.camenzind@uzh.ch>
2025-05-05 19:01:21 +0200

    Updated the castVote method to properly cast votes and provide persistence. Had to also adjust the calculateStatus method to not take the reference to a poi but get the actual poi to calculate the status properly.

--- src/test/java/ch/uzh/ifi/hase/soprafs24/service/PointOfInterestServiceIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/PointOfInterestServiceIntegrationTest.java
@@ -16,6 +16,8 @@ import ch.uzh.ifi.hase.soprafs24.repository.UserRepository;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -550,6 +552,7 @@ public class PointOfInterestServiceIntegrationTest {
 
         @Test
         public void calculateStatus_majorityVoting_success() {
+
                 // Create test users
                 User owner = new User();
                 owner.setUsername("owner");
